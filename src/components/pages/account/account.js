03fb7cf99d484c1fff66e7ff51b1117937b5fca7
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-11-05 09:39:41 -0300

    WIP - Dados da página de usuário
    Adição da visualização de dados para página de usuário, também permitindo retornar ao mapa e fazer logoff.

--- src/components/pages/account/account.js
+++ src/components/pages/account/account.js
@@ -1,16 +1,48 @@
-async function carregarDados() {
-    const response = await fetch('/me');
-    const data = await response.json();
+// Importações
+import Msg from '/utils/msg.js'
 
-    if (data.success) {
-        document.getElementById('nome').textContent = data.user.nome;
-        document.getElementById('email').textContent = data.user.email;
-        document.getElementById('cpf').textContent = data.user.cpf;
-        document.getElementById('cidade').textContent = data.user.endereco;
-    } else {
-        alert('Erro ao carregar dados do usuário');
-        window.location.href = '/Pages/login.html';
+// Variáveis importantes
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const cpf = document.getElementById('cpf')
+const cidade = document.getElementById('cidade')
+const btnReturn = document.getElementById('return')
+const btnLogoff = document.getElementById('logoff')
+
+const sv = 'http://localhost:3000'
+
+// Botão de retorno
+btnReturn.addEventListener('click', () => {
+    window.location.href = '/index'
+})
+
+// Botão de logoff
+btnLogoff.addEventListener('click', () => {
+    sessionStorage.removeItem('nome_user')
+    sessionStorage.removeItem('email_user')
+    sessionStorage.removeItem('cpf_user')
+    sessionStorage.removeItem('endereco_user')
+    window.location.href = '/login.html'
+})
+
+// Dados do usuário
+fetch(`${sv}/usuario/${sessionStorage.getItem('cpf_user')}`)
+.then(res => res.json())
+.then(data => {
+    nome.innerHTML = data[0].nome
+    email.innerHTML = data[0].email
+    cpf.innerHTML = data[0].cpf
+    cidade.innerHTML = data[0].endereco
+})
+.catch(err => {
+    console.log(err)
+    const config = {
+            titulo: "Erro",
+            texto: "Dados não encontrados no sistema!",
+            cor: "#9c0606",
+            tipo: "ok",
+            ok: () => {},
+            confirmar: () => {}
     }
-}
-
-document.addEventListener('DOMContentLoaded', carregarDados);
+    Msg.mostrar(config)
+})
